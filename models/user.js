a9f518b659f7cf493ed8e76f3f3112e4cea08655
timmmartinson <timmmartinson@gmail.com>
2023-01-26 07:36:00 -0700

    Fix user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const monthSchema = require('./month')
+const Month = require('./month')
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
@@ -33,7 +33,9 @@ const userSchema = new mongoose.Schema(
 userSchema.pre('save', function (next) {
     if (!this.isNew) return next() // only run on new documents
     for (let i = 0; i < 12; i++) {
-        this.months.push({ month: months[i] })
+        let newMonth = new Month({month: months[i]})
+        newMonth.save()
+        this.months.push(newMonth._id)
     }
     next()
 })
